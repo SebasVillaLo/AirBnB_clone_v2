5c843dad3b24a3831c773c7f64c8bb117ef5bb3d
122-63 <3469@holbertonschool.com>
2021-12-22 21:37:16 -0500

    DBStorage - States and Cities

---
+++ models/engine/db_storage.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+
+from os import getenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+from models.base_model import Base
+from models.state import State
+from models.user import User
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.amenity import Amenity
+
+
+class DBStorage:
+    """The new engine DBStorage"""
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        user = getenv('HBNB_MYSQL_USER')
+        pwd = getenv('HBNB_MYSQL_PWD')
+        host = getenv('HBNB_MYSQL_HOST')
+        db = getenv('HBNB_MYSQL_DB')
+        env = getenv('HBNB_ENV')
+
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.
+                                      format(user, pwd, host, db),
+                                      pool_pre_ping=True)
+
+        if env == "test":
+            Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        """ Show all class objects in DB storage or specified class """
+        from models.city import City
+        from models.place import Place
+        from models.review import Review
+        from models.amenity import Amenity
+        my_dict = {}
+        classes = [State, City, User, Place, Review, Amenity]
+        if cls:
+            classes = [cls]
+        for j in classes:
+            for ky in self.__session.query(j).all():
+                key = "{}.{}".format(type(ky).__name__, ky.id)
+                my_dict[key] = ky
+        return my_dict
+
+    def new(self, obj):
+        """Create a new object"""
+        self.__session.add(obj)
+
+    def save(self):
+        """ commit all changes of the current database session """
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """dsdcsd """
+        if obj is not None:
+            self.__session.delete(obj)
+
+    def reload(self):
+        """ Create database in Alchemy"""
+        Base.metadata.create_all(self.__engine)
+        Session = sessionmaker()
+        Session.configure(bind=self.__engine)
+        self.__session = Session()
+
+    def close(self):
+        """ Close Session """
+        self.__session.close()
--- models/state.py
+++ models/state.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
-
+from sqlalchemy.sql.sqltypes import String
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql.schema import Column
+import models
+from models.city import City
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    if models.type_storage == "db":
+        __tablename__ = "states"
+        name = Column(String(128), nullable=False)
+        cities = relationship('City')
+    else:
+        name = ''
+        cities = models.storage.all(City)
