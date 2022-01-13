479360872e05340579e25af905cd3c5586071550
SebasVillaLo <villalopezsebastian@gmail.com>
2022-01-13 17:29:14 -0500

    add proyect

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -19,17 +19,19 @@ def do_deploy(archive_path):
     """
     if exists(archive_path) is False:
         return False
-    archive_name = archive_path.split('/')[1]
-    name_archive = archive_name.split('.')[0]
-    re_archive = "/data/web_static/releases/{}".format(name_archive)
-    up_archive = "/tmp/{}".format(archive_name)
-    put(archive_path, up_archive)
-    run('mkdir -p ' + re_archive)
-    run('tar -xzf /tmp/{} -C {}/'.format(archive_name, re_archive))
-    run('rm {}'.format(up_archive))
-    mv = 'mv ' + re_archive + '/web_static/* ' + re_archive + '/'
-    run(mv)
-    run('rm -rf ' + re_archive + '/web_static')
-    run('rm -rf /data/web_static/current')
-    run('ln -s ' + re_archive + ' /data/web_static/current')
-    return True
+    try:
+        nameFile = archive_path.split("/")[-1]
+        NFile_no_ext = nameFile.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, NFile_no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(nameFile, path, NFile_no_ext))
+        run('rm /tmp/{}'.format(nameFile))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, NFile_no_ext))
+        run('rm -rf {}{}/web_static'.format(path, NFile_no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, NFile_no_ext))
+        return True
+    except Exception:
+        return False
+
