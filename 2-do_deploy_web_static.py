d93059cf99ea9cd71a7caf5770606889bc2319ca
SebasVillaLo <villalopezsebastian@gmail.com>
2022-01-13 21:03:21 -0500

    add proyect

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,7 +5,7 @@ Deploying tgz file to our servers
 
 from fabric.api import put, run, env
 from os.path import exists
-env.hosts = ['35.237.25.66', '34.234.71.240']
+env.hosts = ['23.21.15.186', '	54.84.83.106']
 env.user = 'ubuntu'
 
 
