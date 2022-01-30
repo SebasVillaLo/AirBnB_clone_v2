38bff86832f0c71b611f2b098b5022b339e7bddf
SebasVillaLo <villalopezsebastian@gmail.com>
2022-01-30 12:40:49 -0500

    add proyect

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -25,9 +25,9 @@ def CText(text):
     return ("C {}".format(escape(textC)))
 
 
-@app.route('/python/', strict_slashes=False)
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def pythonText(text="is cool"):
+def pythonText(text):
     """Two routes, the firts is for home python, the next route is for the arguments inserted for the user"""
     textC = text.replace('_', ' ')
     return ("Python {}".format(escape(textC)))
