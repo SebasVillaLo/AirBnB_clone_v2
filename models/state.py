62fd50cbed94472304e7f3d5e8dccd656ee196b8
SebasVillaLo <villalopezsebastian@gmail.com>
2022-01-30 14:00:38 -0500

    add proyect

--- models/state.py
+++ models/state.py
@@ -1,28 +1,30 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from sqlalchemy.orm import relationship
-from sqlalchemy.sql.schema import Column
-from sqlalchemy.sql.sqltypes import String
 from models.base_model import BaseModel, Base
-import models
+from sqlalchemy.sql.schema import Column, ForeignKey
+from sqlalchemy.sql.sqltypes import String
+from sqlalchemy.orm import relationship
 from models.city import City
+import models
+from os import getenv
 
 
-class State(BaseModel, Base):
-    """  Implementation for the State """
-    if models.type_storage == "db":
-        __tablename__ = "states"
+if getenv('HBNB_TYPE_STORAGE') == 'db':
+    class State(BaseModel, Base):
+        """ State class """
+        __tablename__ = 'states'
         name = Column(String(128), nullable=False)
-        cities = relationship('City')
-    else:
+        cities = relationship('City', backref='state')
+else:
+    class State(BaseModel):
+        """ State class """
         name = ''
 
         @property
         def cities(self):
-            """This funtions add states at the list"""
-            citiesList = []
-            cities = models.storage.all(City)
-            for city in cities.values():
+            cities_list = []
+            cities = models.storage.all(City).values()
+            for city in cities:
                 if city.state_id == self.id:
-                    citiesList.append(city)
-            return citiesList
+                    cities_list.append(city)
+            return cities_list
